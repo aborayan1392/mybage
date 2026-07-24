aa096a0dad87030fdf43cc58114f5c0b6c60a05a
aborayan1392 <126050178+aborayan1392@users.noreply.github.com>
2026-07-24 14:28:24 +0300

    Cache complete reading tracker PWA for offline use

--- reading-tracker/sw.js
+++ reading-tracker/sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'reading-tracker-cache-v3';
-const APP_SHELL = ['./','./index.html','./payload.js','./manifest.json','./icon.svg','./icon-maskable.svg'];
+const CACHE_NAME = 'reading-tracker-cache-v4';
+const APP_SHELL = ['./','./index.html','./payload.js','./payload-1.txt','./payload-2.txt','./payload-3.txt','./payload-4.txt','./payload-5.txt','./manifest.json','./icon.svg','./icon-maskable.svg'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch', event => {
